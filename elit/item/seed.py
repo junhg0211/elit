f37d36308506406f44f7defbec0fdd43fa1ccf1d
Sch <junhg0211@gmail.com>
2021-08-20 22:25:54 +0900

    planting cancel

--- elit/item/seed.py
+++ elit/item/seed.py
@@ -1,3 +1,4 @@
+from asyncio import TimeoutError as AsyncioTimeoutError
 from typing import Tuple
 
 from discord import Embed
@@ -23,10 +24,17 @@ class Seed(Item):
             raise ChannelError(f':x: {ctx.author.mention} **여기서는 농작물을 심을 수 없어요!** '
                                f'{farm_channel.mention}에서 시도해주세요.')
 
-        await ctx.send(':potted_plant: 심을 농작물의 이름을 입력해주세요.')
-        message = await bot.wait_for('message', timeout=30)
-        # except: raise f':x: {ctx.author.mention} **작물 심기가 취소되었습니다.** 작물 이름 입력 시간이 초과되었어요...'
-        crop_name = message.content
+        await ctx.send(f':potted_plant: 심을 농작물의 이름을 입력해주세요. 농작물은 __{amount}개__를 심습니다. '
+                       f'(`취소` 또는 `cancel`이라고 입력하면 심기가 취소됩니다.)')
+        try:
+            message = await bot.wait_for('message', timeout=15)
+        except AsyncioTimeoutError:
+            raise AsyncioTimeoutError(f':x: {ctx.author.mention} **작물 심기가 취소되었습니다.** '
+                                      f'작물 이름 입력 시간이 초과되었어요...')
+        else:
+            if message.content in ('취소', 'cancel'):
+                raise ValueError(':x: **심기를 취소했습니다.**')
+            crop_name = message.content
 
         if byte_len(crop_name) > 16:
             raise ValueError(f':x: {ctx.author.mention} **작물 이름이 너무 길어요!** '
@@ -39,4 +47,4 @@ class Seed(Item):
         embed.add_field(name='심은 개수', value=amount)
         embed.add_field(name='심은 날짜', value=str(planted_at))
 
-        return self.apply_use(amount, f':potted_plant: {farm_channel.mention}에 `{crop_name}` {amount}개를 심었습니다.'), embed
+        return self.apply_use(amount, f':potted_plant: {farm_channel.mention}에 `{crop_name}` __{amount}개__를 심었습니다.'), embed
