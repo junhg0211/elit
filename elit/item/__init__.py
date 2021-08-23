f23c9f69fd7db3c314417b4519f9b73357031e3d
junhg0211 <junhg0211@gmail.com>
2021-08-23 15:53:25 +0900

    implementation of various check items

--- elit/item/__init__.py
+++ elit/item/__init__.py
@@ -1,5 +1,5 @@
 from .item import Item, Item1
 
-from .check import Check10
+from .check import Check10, Check100, Check1000, Check10000
 from .seed import Seed
 from .crop import Crop
--- elit/item/check.py
+++ elit/item/check.py
@@ -16,3 +16,45 @@ class Check10(Item):
         player.earn_money(10 * amount)
         return self.apply_use(amount, f'{self.name}{eul_reul(self.name)} {amount}번 사용해서 '
                                       f'{10 * amount}{currency}{eul_reul(currency)} 얻었다!!')
+
+
+class Check100(Item):
+    name = '100원짜리 수표'
+    type = 5
+    description = f'100원짜리 수표입니다. ' \
+                  f'사용하면 100{const("currency.default")}{eul_reul(const("currency.default"))} 받을 수 있습니다.'
+
+    async def use(self, amount: int, player, bot: Bot, ctx: Context) -> str:
+        self.check_amount(amount)
+        currency = const("currency.default")
+        player.earn_money(100 * amount)
+        return self.apply_use(amount, f'{self.name}{eul_reul(self.name)} {amount}번 사용해서 '
+                                      f'{100 * amount}{currency}{eul_reul(currency)} 얻었다!!')
+
+
+class Check1000(Item):
+    name = '1000원짜리 수표'
+    type = 6
+    description = f'1000원짜리 수표입니다. ' \
+                  f'사용하면 1000{const("currency.default")}{eul_reul(const("currency.default"))} 받을 수 있습니다.'
+
+    async def use(self, amount: int, player, bot: Bot, ctx: Context) -> str:
+        self.check_amount(amount)
+        currency = const("currency.default")
+        player.earn_money(1000 * amount)
+        return self.apply_use(amount, f'{self.name}{eul_reul(self.name)} {amount}번 사용해서 '
+                                      f'{1000 * amount}{currency}{eul_reul(currency)} 얻었다!!')
+
+
+class Check10000(Item):
+    name = '10000원짜리 수표'
+    type = 7
+    description = f'10000원짜리 수표입니다. ' \
+                  f'사용하면 10000{const("currency.default")}{eul_reul(const("currency.default"))} 받을 수 있습니다.'
+
+    async def use(self, amount: int, player, bot: Bot, ctx: Context) -> str:
+        self.check_amount(amount)
+        currency = const("currency.default")
+        player.earn_money(10000 * amount)
+        return self.apply_use(amount, f'{self.name}{eul_reul(self.name)} {amount}번 사용해서 '
+                                      f'{10000 * amount}{currency}{eul_reul(currency)} 얻었다!!')
